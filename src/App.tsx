16c621899c242f5b041e92c6e6e7c62c04a80e7d
Alireza Aberoumand <alirezaaberoumand7192@gmail.com>
2024-06-22 21:03:19 -0700

    Save changes for today 06/22/2024

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='body'>
+      <div className="project-main-container">
+        <header className="header--container">
+          <section className="header-nav-container">
+            <div className="nav--logo-container">
+              <h4 className='nav-logo-h4'>Learning</h4>
+            </div>
+            <nav className="nav--links-container">
+              <li className="nav--link--li"><a href="#" className="nav-link-a">Home</a></li>
+              <li className="nav--link--li"><a href="#" className="nav-link-a">About Us</a></li>
+              <li className="nav--link--li"><a href="#" className="nav-link-a">Services</a></li>
+              <li className="nav--link--li"><a href="#" className="nav-link-a">Courses</a></li>
+              <li className="nav--link--li"><a href="#" className="nav-link-a">Blog</a></li>
+              <li className="nav--link--li"><a href="#" className="nav-link-a active">Register</a></li>
+            </nav>
+          </section>
+        </header>
+      </div>
+    </div>
   )
 }
 
